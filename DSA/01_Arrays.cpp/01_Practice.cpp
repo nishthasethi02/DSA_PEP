828b7871a88eb3cb799ab9ec79375a431b291502
Nishtha Sethi <nishthasethi004@gmail.com>
2026-02-02 09:13:52 +0530

    Done

--- DSA/01_Arrays.cpp/01_Practice.cpp
+++ DSA/01_Arrays.cpp/01_Practice.cpp
@@ -52,3 +52,26 @@
 //         return false;
 //     }
 // };
+
+// https://www.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
+// class Solution {
+//   public:
+//     void sort012(vector<int>& arr) {
+//         // code here
+//         int low = 0;
+//         int mid = 0;
+//         int high = arr.size() - 1;
+//         while(mid <= high){
+//             if(arr[mid]==0){
+//                 swap(arr[low++], arr[mid++]);
+//             }
+//             else if(arr[mid]==1){
+//                 mid++;
+//             }
+//             else{
+//                 swap(arr[high], arr[mid]);
+//                 high--;
+//             }
+//         }
+//     }
+// };
