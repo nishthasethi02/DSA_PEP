6a26d4c687a127727d833da7ac6403481f6871f7
Nishtha Sethi <nishthasethi004@gmail.com>
2026-02-03 11:04:20 +0530

    Ques

---
+++ DSA/Ques.cpp/0242-valid-anagram.cpp
@@ -0,0 +1,21 @@
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if (s.length() != t.length()){
+//             return false;
+//         }
+//         int arr[26] = {0};
+
+//         for(char i : s){
+//             arr[i - 'a']++;
+//         }
+
+//         for(char i : t){
+//             arr[i - 'a']--;
+//             if(arr[i-'a'] < 0){
+//                 return false;
+//             }
+//         }    
+//         return true;
+//     }
+// };
