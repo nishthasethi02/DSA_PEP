3f93b5e04a77abb0472820056773e65041d3461f
Nishtha Sethi <nishthasethi004@gmail.com>
2026-02-05 16:56:55 +0530

    Ques

--- DSA/05_LinkedList.cpp/01_Ques.cpp
+++ DSA/05_LinkedList.cpp/01_Ques.cpp
@@ -5,3 +5,19 @@
 // https://leetcode.com/problems/palindrome-linked-list/description/
 
 // https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+// https://www.geeksforgeeks.org/problems/insert-in-a-sorted-list/1
+
+// https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+
+// https://leetcode.com/problems/linked-list-cycle/
+// -> Floyd Loop Detection Algorithm
+
+// https://leetcode.com/problems/linked-list-cycle-ii/description/
+
+// https://leetcode.com/problems/odd-even-linked-list/description/
+
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+// https://leetcode.com/problems/partition-list/submissions/1909036714/
+
