a6fe2cde388eafd3a7dd661725e34e957d8c8afd
Nishtha Sethi <nishthasethi004@gmail.com>
2026-02-06 13:24:56 +0530

    today's

--- DSA/05_LinkedList.cpp/01_Ques.cpp
+++ DSA/05_LinkedList.cpp/01_Ques.cpp
@@ -21,3 +21,34 @@
 
 // https://leetcode.com/problems/partition-list/submissions/1909036714/
 
+// https://www.geeksforgeeks.org/problems/given-a-linked-list-of-0s-1s-and-2s-sort-it/1
+// Count approach 
+
+// reverse in k groups
+// https://leetcode.com/problems/reverse-nodes-in-k-group/
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        //count k nodes
+        ListNode* curr = head;
+        int count = 0;
+        while(curr != NULL && count != k){ // count != k because we need to check if we have k nodes
+            curr = curr->next;
+            count++;
+        }
+        if(count == k){
+            //reverse k nodes
+            curr = reverseKGroup(curr, k); //reverse rest of the list
+            
+            //reverse current k nodes
+            while(count > 0){
+                ListNode* nextNode = head->next;
+                head->next = curr;
+                curr = head;
+                head = nextNode;
+                count--;
+            }
+            head = curr;
+        }
+    }
+};
