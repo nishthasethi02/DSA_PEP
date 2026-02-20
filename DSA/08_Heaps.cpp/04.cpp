7b1dc5e3a035423091a7f316a0c4c09cd775f694
Nishtha Sethi <nishthasethi004@gmail.com>
2026-02-20 15:47:50 +0530

    Ques

---
+++ DSA/08_Heaps.cpp/04.cpp
@@ -0,0 +1,39 @@
+/* 
+Priority Queue -> It is a special type of queue where elements are accessed on basis of priority, not the insertion order
+               -> By default it has high
+               -> internally it is 
+
+
+*/
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+    // (max heap)
+    priority_queue<int> pq_max;
+
+    // (min heap)
+    priority_queue<int, vector<int>, greater<int>> pq_min;
+
+    pq_max.push(10);
+    pq_max.push(5);
+    pq_max.push(20);
+
+    cout << pq_max.size() << endl;
+
+    while(!pq_max.empty()){
+        cout << pq_max.top() << " ";
+        pq_max.pop();
+    }
+
+    pq_max.pop();
+}
+
+// https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+// https://www.geeksforgeeks.org/problems/k-largest-elements4206/1
+
+// https://www.geeksforgeeks.org/problems/k-largest-elements4206/1
+
