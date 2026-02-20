0f34274eefac22dbb53892954c90b93ba47e4686
Nishtha Sethi <nishthasethi004@gmail.com>
2026-02-20 15:50:30 +0530

    Ques

--- DSA/Ques.cpp/0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ DSA/Ques.cpp/0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,8 +1,25 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.end());
-        return nums[k-1];
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+
+        for(int i : nums){
+            minHeap.push(i);
+
+            if(minHeap.size() > k){
+                minHeap.pop();
+            }
+        }
+
+        return minHeap.top();
     }
 };
+
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         sort(nums.begin(), nums.end());
+//         reverse(nums.begin(), nums.end());
+//         return nums[k-1];
+//     }
+// };
