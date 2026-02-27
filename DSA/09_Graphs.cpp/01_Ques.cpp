dcb95287ea9dbb0f318a86b3d9802fcfa6bdbcd3
Nishtha Sethi <nishthasethi004@gmail.com>
2026-02-27 14:44:29 +0530

    Ques

--- DSA/09_Graphs.cpp/01_Ques.cpp
+++ DSA/09_Graphs.cpp/01_Ques.cpp
@@ -254,3 +254,67 @@
 // https://leetcode.com/problems/rotting-oranges/description/
 
 // https://leetcode.com/problems/surrounded-regions/description/
+
+// https://www.naukri.com/code360/problems/shortest-path-in-an-unweighted-graph_981297
+
+// https://www.naukri.com/code360/problems/shortest-path-in-dag_8381897
+// void dfs(int node, vector<vector<pair<int, int>>> &adj, vector<int> &visited, stack<int> &st){
+//     visited[node] = 1;
+//     for(auto &i : adj[node]){
+//         int neighbor = i.first;
+//         if(!visited[neighbor]){
+//             dfs(neighbor, adj, visited, st);
+//         }
+//     }
+//     // after visited all neighbors, push current node into stack(from herei cannot go further to any neighbor)
+//     st.push(node);
+// }
+// vector<int> shortestPathInDAG(int n, int m, vector<vector<int>> &edges)
+// {
+//     // Write your code here
+//     vector<vector<pair<int, int>>> adj(n);
+
+//     for(int i = 0; i < m; i++){
+//         int u = edges[i][0];
+//         int v = edges[i][1];
+//         int w = edges[i][2];
+//         adj[u].push_back({v,w});
+
+//     }
+
+//     vector<int> visited(n, 0);
+//     stack<int> st;
+
+//     for(int i = 0; i < n; i++){
+//         if(!visited[i]){
+//             // dfs
+//             dfs(i, adj, visited, st);
+//         }
+//     }
+
+//     vector<int> distance(n, INT_MAX);
+//     distance[0] = 0;
+//     while(!st.empty()){
+//         int node = st.top();
+//         st.pop();
+
+//         if(distance[node] != INT_MAX){
+//             for(auto &edge : adj[node]){
+//                 int neighbor = edge.first;
+//                 int weight = edge.second;
+
+//                 if(distance[node] + weight < distance[neighbor]){
+//                     distance[neighbor] = distance[node] + weight;
+//                 }
+//             }
+//         } 
+//     }
+
+//     for(int i = 0; i < n; i++){
+//         if(distance[i] == INT_MAX){
+//             distance[i] = -1;
+//         }
+//     }
+
+//     return distance;
+// }
