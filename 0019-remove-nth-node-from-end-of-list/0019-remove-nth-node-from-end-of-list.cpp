b39f1ce77237aca13fd536722920b0726f009f49
nishthasethi02 <152066039+nishthasethi02@users.noreply.github.com>
2026-03-31 22:04:58 +0530

    Time: 0 ms (100%), Space: 14.9 MB (58.57%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,76 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+
+        ListNode* slow = dummy;
+        ListNode* fast = dummy;
+
+        for(int i = 0; i < n ;i ++){
+            fast = fast->next;
+        }
+
+        while(fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        ListNode* toDel = slow->next;
+        slow->next = slow->next->next;
+        delete toDel;
+
+        return dummy->next;
+    }
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// class Solution {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         // window approach
+//         ListNode* fast = head;
+//         ListNode* slow = head;
+
+//         // move fast ahead by n steps
+//         for(int i = 0; i < n; i++){
+//             fast=fast->next;
+//         }
+
+//         // if fast is NULL
+//         if(fast == NULL){
+//             return head->next;
+//         }
+
+//         while(fast->next != NULL){
+//             fast=fast->next;
+//             slow=slow->next;
+//         }
+
+//         // slow->next is the node to be rmoved
+//         ListNode* toDel = slow->next;
+//         slow->next = slow->next->next;
+//         delete toDel;
+
+//         return head;
+//     }
+// };
