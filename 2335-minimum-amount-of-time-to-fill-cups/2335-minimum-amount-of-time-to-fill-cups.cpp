d66cca3db820d8c20da2b4dea8e25ceb2484f9a0
nishthasethi02 <152066039+nishthasethi02@users.noreply.github.com>
2026-04-08 22:56:30 +0530

    Time: 0 ms (100%), Space: 14.1 MB (79.31%) - LeetHub

---
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int fillCups(vector<int>& amount) {
+        int sum = amount[0] + amount[1] + amount[2];
+        int maxi = max({amount[0], amount[1], amount[2]});
+        
+        return max(maxi, (sum + 1) / 2);
+    }
+};
