36dfc8417a6465d8804fc4a1e4632ecf20d9e229
nishthasethi02 <152066039+nishthasethi02@users.noreply.github.com>
2026-04-11 16:49:59 +0530

    Time: 4 ms (49.88%), Space: 118.1 MB (64.69%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -13,83 +13,67 @@ public:
     bool isPalindrome(ListNode* head) {
         ListNode* slow = head;
         ListNode* fast = head;
-        
-        while(fast != NULL && fast->next != NULL){
+
+        while(fast!=NULL && fast->next!=NULL){
             slow = slow->next;
             fast = fast->next->next;
         }
 
         ListNode* prev = NULL;
         ListNode* curr = slow;
-        ListNode* nextnode = NULL;
+        ListNode* next = NULL;
 
         while(curr != NULL){
-            nextnode = curr->next;
+            next = curr->next;
             curr->next = prev;
             prev = curr;
-            curr = nextnode;
+            curr = next;
         }
 
         ListNode* p1 = head;
         ListNode* p2 = prev;
-
-        while(p2 != NULL){
-            if(p1->val != p2->val){
-                return false;
+        while(p2!=NULL){
+            if(p1->val!=p2->val){
+            return false;  
             }
-            p1 = p1->next;
-            p2 = p2->next;
+            p1=p1->next;
+            p2=p2->next;
         }
         return true;
     }
 };
 
-// /**
-//  * Definition for singly-linked list.
-//  * struct ListNode {
-//  *     int val;
-//  *     ListNode *next;
-//  *     ListNode() : val(0), next(nullptr) {}
-//  *     ListNode(int x) : val(x), next(nullptr) {}
-//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-//  * };
-//  */
 // class Solution {
 // public:
-//     ListNode* reverse(ListNode* head){
-//         ListNode* prev = NULL;
-//         ListNode* curr = head;
-//         ListNode* next = NULL;
-
-//         while(curr!=NULL){
-//             next = curr->next;
-//             curr->next = prev;
-//             prev = curr;
-//             curr = next;
-//         }
-//         return prev;
-//     }
-
 //     bool isPalindrome(ListNode* head) {
 //         ListNode* slow = head;
 //         ListNode* fast = head;
-
+        
 //         while(fast != NULL && fast->next != NULL){
 //             slow = slow->next;
 //             fast = fast->next->next;
 //         }
 
-//         slow = reverse(slow);
+//         ListNode* prev = NULL;
+//         ListNode* curr = slow;
+//         ListNode* nextnode = NULL;
+
+//         while(curr != NULL){
+//             nextnode = curr->next;
+//             curr->next = prev;
+//             prev = curr;
+//             curr = nextnode;
+//         }
 
 //         ListNode* p1 = head;
-//         ListNode* p2 = slow;
+//         ListNode* p2 = prev;
 
-//         while(p2!=NULL){
-//         if(p1->val!=p2->val){
-//             return false;
-//         }
-//         p1=p1->next;
-//         p2=p2->next;
+//         while(p2 != NULL){
+//             if(p1->val != p2->val){
+//                 return false;
+//             }
+//             p1 = p1->next;
+//             p2 = p2->next;
 //         }
 //         return true;
 //     }
