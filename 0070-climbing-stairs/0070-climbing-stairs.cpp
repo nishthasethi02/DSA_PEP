eb473ef76fb8366ae5fafa3f2be2b680ee7d6d68
nishthasethi02 <152066039+nishthasethi02@users.noreply.github.com>
2026-08-13 01:13:53 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
-    int helper(int n, vector<int> &dp){
-        if ( n == 1 || n == 2) return n;
-        if (dp[n] != -1){
-            return dp[n];
-        }
-        return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
-    }
     int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
-        return helper(n, dp);
+        if(n == 1 || n == 2) return n;
+
+        vector<int> dp(n+1);
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i <= n ; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
+
+// Memoization
+// class Solution {
+// public:
+//     int helper(int n, vector<int> &dp){
+//         if ( n == 1 || n == 2) return n;
+//         if (dp[n] != -1){
+//             return dp[n];
+//         }
+//         return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
+//     }
+//     int climbStairs(int n) {
+//         vector<int> dp(n+1, -1);
+//         return helper(n, dp);
+//     }
+// };
