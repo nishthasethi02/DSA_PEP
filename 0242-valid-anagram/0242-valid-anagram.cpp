67131df374860def099be1c493ce34cbc1dd8045
nishthasethi02 <152066039+nishthasethi02@users.noreply.github.com>
2026-08-15 03:25:38 +0530

    Time: 0 ms (100%), Space: 9.6 MB (92.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,16 +2,36 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size() != t.size()) return false;
+
         vector<int> freq(26,0);
-        for(int i = 0; i < s.size(); i++){
+
+        for(int i = 0; i <s.size(); i++){
             freq[s[i] - 'a']++;
             freq[t[i] - 'a']--;
         }
         for(int x : freq){
-            if(x!=0){
+            if(x != 0){
                 return false;
             }
         }
         return true;
     }
 };
+
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if(s.size() != t.size()) return false;
+//         vector<int> freq(26,0);
+//         for(int i = 0; i < s.size(); i++){
+//             freq[s[i] - 'a']++;
+//             freq[t[i] - 'a']--;
+//         }
+//         for(int x : freq){
+//             if(x!=0){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
