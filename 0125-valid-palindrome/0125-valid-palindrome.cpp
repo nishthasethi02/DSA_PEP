4f947f1aaae582a1fc2b9c95e3273e9340a5ab4a
nishthasethi02 <152066039+nishthasethi02@users.noreply.github.com>
2026-08-17 00:23:41 +0530

    Time: 0 ms (100%), Space: 10.1 MB (30.09%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -7,15 +7,37 @@ public:
                 temp += tolower(c);
             }
         }
-        int left = 0;
-        int right = temp.size() - 1;
-        while(left < right){
-            if(temp[left] != temp[right]){
+        int i = 0;
+        int j = temp.size() - 1;
+        while(i < j){
+            if(temp[i] != temp[j]){
                 return false;
             }
-            left++;
-            right--;
+            i++;
+            j--;
         }
         return true;
     }
 };
+
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+//         string temp = "";
+//         for(char c : s){
+//             if(isalnum(c)){
+//                 temp += tolower(c);
+//             }
+//         }
+//         int left = 0;
+//         int right = temp.size() - 1;
+//         while(left < right){
+//             if(temp[left] != temp[right]){
+//                 return false;
+//             }
+//             left++;
+//             right--;
+//         }
+//         return true;
+//     }
+// };
