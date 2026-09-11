cf44e0a55b3846b5f1c320b522a8cff4419f6b51
nishthasethi02 <152066039+nishthasethi02@users.noreply.github.com>
2026-09-12 01:30:13 +0530

    Time: 3 ms (82.58%), Space: 13.7 MB (74.38%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     string removeDuplicates(string s) {
         string result = "";
-
         for(char c : s){
             if(!result.empty() && result.back() == c){
-                result.pop_back(); // remove duplicate
-            } else {
+                result.pop_back();
+            }else{
                 result.push_back(c);
             }
         }
-
         return result;
     }
 };
